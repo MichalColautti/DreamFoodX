9f35443b5229398f4e97368080c49f675948c4bf
MichalC <m.colautti@o2.pl>
2025-05-07 19:20:41 +0200

    Add database for users

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,6 @@
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    item_name VARCHAR(100) NOT NULL,
-    item_description TEXT,
-    price DECIMAL(10,2),
-    item_size ENUM('S', 'M', 'L', 'XL') NOT NULL,
-    color VARCHAR(50),
-    amount INT DEFAULT 0
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
 );
